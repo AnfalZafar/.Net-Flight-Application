25e277b3059557e2a2271c823a1f1d90eb013c80
Anfal Zafar <anfalzafar533@gmail.com>
2024-06-12 12:42:07 -0700

    AirLine Project with ASP.Net

--- Flight/obj/Debug/net6.0/Flight.AssemblyInfo.cs
+++ Flight/obj/Debug/net6.0/Flight.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ab25fcc02ae170ca714bd195fc47a28dbd744b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0e8625f1b152291d9501e367bfea7ab2984034")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
